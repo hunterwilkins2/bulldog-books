f33f8375da1eca68ed309d8a2b7b68f4d60802b1
GaryBarnes13 <gary.barnesg@gmail.com>
2021-02-23 09:05:57 -0500

    Installed Formik and started on Profile Page

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Formik } from 'formik'
+import { Form, Button } from 'react-bootstrap'
+import StoreNavbar from '../StoreNavbar'
+import '../../styles.css'
+
+// pick back up at 2:37 on the video
 
 function Profile(){
     return(
-        <h1>Profile Page!</h1>
+        <>
+            <StoreNavbar type="user"/>
+            <div style={{display: 'flex', justifyContent: 'center', maxWidth: '100vw'}}>
+                <Formik initialValues={{}}>{() => (
+                    <form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit"> Submit </Button>
+                    </form>
+                )}</Formik>
+            </div>
+        </>
     )
 }
-
 export default Profile
