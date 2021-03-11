51ecf6e3c6c1803e9f5fea951c68916eed3845fa
GaryBarnes13 <gary.barnesg@gmail.com>
2021-03-11 15:33:46 -0800

    added formik and yup to login page

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,22 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'node': true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended'
-    ],
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        'ecmaVersion': 12,
-        'sourceType': 'module'
+        ecmaVersion: 12,
+        sourceType: 'module',
     },
-    'plugins': [
-        'react'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
+    plugins: ['react'],
+    rules: {
+        indent: ['error', 4],
         'linebreak-style': 0,
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
-    }
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+    },
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Form, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { Formik, Field } from 'formik'
+import * as yup from 'yup'
 
 import BasicNav from './BasicNav'
 import './styles/Login.css'
@@ -13,48 +15,76 @@ function Login(){
         borderRadius: '25px'
     }
 
+    const schema = yup.object().shape({
+        email: yup.string().email('Invalid Email Format').required('Required'),
+        password: yup.string().required('Required')
+    })
+    
+
     return(
         <>
             <BasicNav />
             <div style={{display: 'flex', justifyContent: 'center', maxWidth: '100vw'}}>
-                <Form className="login-form" style={formStyle}>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="email@example.com" />
-                        <Form.Text className="text-muted">
-                            We will never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="password1234" />
-                    </Form.Group>
-                    <Link to='/register'>Register</Link>
-                    <br></br>
-                    <Link to='/forgot'>Forgot Password?</Link>
-                    <br></br>
-                    <Link to={{
-                        pathname: '/user',
-                        userProps:{
-                            admin: false
-                        }
-                    }}>Continue Without an Account</Link>
-                    <br></br>
-                    <Link to={{
-                        pathname: '/admin',
-                        userProps:{
-                            admin: true
-                        }
-                    }}>Continue as Admin</Link>
-                    <br></br>
-                    <br></br>
-                    <Button variant="primary" type="submit">
-                    Submit
-                    </Button>
-                </Form>
+                <Formik 
+                    initialValues={{email: '', password: ''}} 
+                    onSubmit={(data, {setSubmitting}) => {
+                        setSubmitting(true)
+                        // make async call to DB eventually
+                        console.log(data)
+                        setSubmitting(false)
+                    }}
+                    validationSchema={schema}
+                >{({values, errors, setSubmitting, handleSubmit}) => (
+                        <Form className="login-form" style={formStyle} onSubmit={handleSubmit}>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Field 
+                                    placeholder="email@example.com" 
+                                    name='email' as={Form.Control} 
+                                    isInvalid={errors.email}/>
+                                <Form.Text className="text-muted">
+                                    We will never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Field 
+                                    placeholder="password" 
+                                    name='password' 
+                                    type='password' 
+                                    as={Form.Control}
+                                    isInvalid={errors.password}/>
+                            </Form.Group>
+                            <Link to='/register'>Register</Link>
+                            <br></br>
+                            <Link to='/forgot'>Forgot Password?</Link>
+                            <br></br>
+                            <Link to={{
+                                pathname: '/user',
+                                userProps:{
+                                    admin: false
+                                }
+                            }}>Continue Without an Account</Link>
+                            <br></br>
+                            <Link to={{
+                                pathname: '/admin',
+                                userProps:{
+                                    admin: true
+                                }
+                            }}>Continue as Admin</Link>
+                            <br></br>
+                            <br></br>
+                            <Button disabled={setSubmitting} variant="primary" type="submit">
+                                Submit
+                            </Button>
+                            <pre>{JSON.stringify(values, null, 2)}</pre>   
+                            <pre>{JSON.stringify(errors, null, 2)}</pre>     
+                        </Form>
+                    )}</Formik>
             </div>
         </>
     )
 }
 
 export default Login
+
