50c03c68d34e03f1fb24231725e8ac15efaa2a2f
GaryBarnes13 <gary.barnesg@gmail.com>
2021-03-14 12:06:53 -0700

    setup formik & yup on login to display error messages

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import {Form, Button, } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import { Formik, Field } from 'formik'
+import { Formik, Field, ErrorMessage } from 'formik'
 import * as yup from 'yup'
 
 import BasicNav from './BasicNav'
@@ -64,52 +65,53 @@ function Login(){
                         setSubmitting(false)
                     }}
                     validationSchema={schema}
-                >{({values, errors, setSubmitting, handleSubmit}) => (
+                >{({ handleSubmit,
+                        handleChange,
+                        handleBlur,
+                        values,
+                        touched,
+                        errors,
+                        setSubmitting
+                    }) => (
                         <Form className="login-form" style={formStyle} onSubmit={handleSubmit}>
                             <h1> Login </h1>
                             <Form.Group controlId="formBasicEmail">
                                 <Form.Label>Email address</Form.Label>
-                                <Field 
-                                    placeholder="email@example.com" 
-                                    name='email' as={Form.Control} 
-                                    isInvalid={errors.email}/>
-                                <Form.Text className="text-muted">
-                                    We will never share your email with anyone else.
-                                </Form.Text>
+                                <Form.Control 
+                                    name="email"
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    isValid={touched.email && !errors.email}
+                                    isInvalid={touched.email && errors.email}
+                                />
+                                <ErrorMessage name="email" />
                             </Form.Group>
                             <Form.Group controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Field 
-                                    placeholder="password" 
-                                    name='password' 
-                                    type='password' 
-                                    as={Form.Control}
-                                    isInvalid={errors.password}/>
+                                <Form.Control 
+                                    name='password'
+                                    type='password'
+                                    value={values.password}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    isValid={touched.password && !errors.password}
+                                    isInvalid={touched.password && errors.password}
+                                />
+                                <ErrorMessage name="password" /> 
                             </Form.Group>
                             <Link to='/register'>Register</Link>
                             <br></br>
                             <Link to='/forgot'>Forgot Password?</Link>
                             <br></br>
-                            <Link to={{
-                                pathname: '/user',
-                                userProps:{
-                                    admin: false
-                                }
-                            }}>Continue Without an Account</Link>
+                            <Link to='user'>Continue Without an Account</Link>
                             <br></br>
-                            <Link to={{
-                                pathname: '/admin',
-                                userProps:{
-                                    admin: true
-                                }
-                            }}>Continue as Admin</Link>
+                            <Link to='/admin'>Continue as Admin</Link>
                             <br></br>
                             <br></br>
                             <Button disabled={setSubmitting} variant="primary" type="submit">
                                 Submit
-                            </Button>
-                            <pre>{JSON.stringify(values, null, 2)}</pre>   
-                            <pre>{JSON.stringify(errors, null, 2)}</pre>     
+                            </Button>   
                         </Form>
                     )}</Formik>
                 <Form className="login-form" style={formStyle}>
