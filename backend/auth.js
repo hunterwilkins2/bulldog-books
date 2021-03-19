9e642708ee908fe3264b7cf89ecf9c756be1808b
Hunter Wilkins <hunter.wilkins2@gmail.com>
2021-03-19 11:39:34 -0400

    Added functions to verify user and admin

--- backend/auth.js
+++ backend/auth.js
@@ -9,7 +9,58 @@ const createToken = (id, status, type) => {
     })
 }
 
+const verifyCustomer = (req, res, next) => {
+    const token = req.cookies.jwt
+
+    // check if token exist and is valid
+    if(token) {
+        jwt.verify(token, process.env.RSA_PRIVATE, (error, decodedToken) => {
+            if(error) {
+                res.status(401).json( { errors: 'Must be logged in'})
+                res.redirect('/')
+            } else {
+                if(decodedToken.status !== 'active') {
+                    res.status(403)
+                    res.redirect('/confirmation')
+                }
+
+                next()
+            }
+        })
+    } else {
+        res.status(401).json( { errors: 'Must be logged in'})
+        res.redirect('/')
+    }
+}
+
+const verifyAdmin = (req, res, next) => {
+    const token = req.cookies.jwt
+
+    // check if token exist and is valid
+    if(token) {
+        jwt.verify(token, process.env.RSA_PRIVATE, (error, decodedToken) => {
+            if(error) {
+                res.redirect(401, '/')
+            } else {
+                if(decodedToken.userType !== 'admin') {
+                    res.redirect('/')
+                }
+
+                if(decodedToken.status !== 'active') {
+                    res.redirect(403, '/confirmation')
+                }
+                
+                next()
+            }
+        })
+    } else {
+        res.redirect(401, '/')
+    }
+}
+
 module.exports = {
     createToken,
+    verifyCustomer,
+    verifyAdmin,
     maxAge
 }
