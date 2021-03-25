1bfb1e7c9643f3f8a455b4d9fb29cf7acee0091b
Hunter Wilkins <hunter.wilkins2@gmail.com>
2021-03-25 17:25:44 -0400

    Added address, payment, and profile router to server

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,12 @@ require('dotenv').config()
 const middlewares = require('./middlewares')
 const books = require('./api/routes/books')
 const register = require('./api/routes/register')
+const payment = require('./api/routes/payment')
+const address = require('./api/routes/address')
+const profile = require('./api/routes/profile')
 
+mongoose.set('useFindAndModify', false)
+// mongoose.set('useCreateIndex', true)
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -26,15 +31,17 @@ app.use(cors({
     credentials: true, origin: process.env.CORS_ORIGIN
 }))
 
-// TODO: change get route to return books
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello, World'
     })
 })
 
-app.use('/api/books', books)
 app.use('/', register)
+app.use('/api/books', books)
+app.use('/api/address', address)
+app.use('/api/payment', payment)
+app.use('/api/profile', profile)
 
 // Error handlers for invalid requests
 app.use(middlewares.notFound)
