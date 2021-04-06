3d10e8dfafd1f85d8bf248a65fa3496d1e558f77
GaryBarnes13 <gary.barnesg@gmail.com>
2021-04-06 12:16:13 -0400

    Fixed all errors/warnings on HomePage except for one deprecation warning

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -11,7 +11,7 @@ import './styles/Homepage.css'
 import './styles/Background.css'
 
 
-function User(){
+function HomePage(){
     const [errors, setErrors] = useState([])
 
     const alerts = errors.map(error => 
@@ -35,34 +35,32 @@ function User(){
     }, [])
 
     const bookCards = books.map(book => (
-        <>
-            <Col key={book.isbn} xs='3' id = "column-hp">
-                <Card id = "card-style-hp">
-                    <Card.Img className = "mx-auto" id = "image-hp" src={book.cover} />
-                    <ListGroup id = "lG-hp" className="list-group-flush" >
-                        <ListGroupItem id = "lGI-title-hp">
-                            <Card.Title>{book.title}</Card.Title>
-                            <Card.Subtitle className="text-muted">{book.subtitle}</Card.Subtitle>
-                        </ListGroupItem>
-                    </ListGroup>  
-                    <ListGroup id = "lG-hp" className="list-group-flush">
-                        <ListGroupItem id = "lGI-hp">Author: {book.author}</ListGroupItem>
-                    </ListGroup>
-                    <ListGroup id = "lG-hp" className="list-group-flush">
-                        <ListGroupItem id = "lGI-hp">Price: ${book.buyPrice}</ListGroupItem>
-                    </ListGroup>
-                    <ListGroup className="list-group-flush">
-                        <ListGroupItem id = "lGI-links-hp">
-                            {Cookies.get('userType') === 'admin' && 
+        <Col key={book.isbn} xs='3' id = "column-hp">
+            <Card id = "card-style-hp">
+                <Card.Img className = "mx-auto" id = "image-hp" src={book.cover} />
+                <ListGroup id = "lG-hp" className="list-group-flush" >
+                    <ListGroupItem id = "lGI-title-hp">
+                        <Card.Title>{book.title}</Card.Title>
+                        <Card.Subtitle className="text-muted">{book.subtitle}</Card.Subtitle>
+                    </ListGroupItem>
+                </ListGroup>  
+                <ListGroup id = "lG-hp" className="list-group-flush">
+                    <ListGroupItem id = "lGI-hp">Author: {book.author}</ListGroupItem>
+                </ListGroup>
+                <ListGroup id = "lG-hp" className="list-group-flush">
+                    <ListGroupItem id = "lGI-hp">Price: ${book.buyPrice}</ListGroupItem>
+                </ListGroup>
+                <ListGroup className="list-group-flush">
+                    <ListGroupItem id = "lGI-links-hp">
+                        {Cookies.get('userType') === 'admin' && 
                              <Card.Link href="">Manage Book</Card.Link>
-                            }
-                            <Card.Link href={book.website}>More Info</Card.Link>
-                            <Card.Link href="">Add To Cart</Card.Link>
-                        </ListGroupItem>                   
-                    </ListGroup>
-                </Card>
-            </Col>
-        </>
+                        }
+                        <Card.Link href={book.website}>More Info</Card.Link>
+                        <Card.Link href="">Add To Cart</Card.Link>
+                    </ListGroupItem>                   
+                </ListGroup>
+            </Card>
+        </Col>
 
     ))
 
@@ -91,4 +89,4 @@ function User(){
     
 }
 
-export default User
+export default HomePage
