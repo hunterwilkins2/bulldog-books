0ad8b078c7be7d88104a5054e3a37744f120f58d
nssafir <nssafir@gmail.com>
2021-04-19 12:10:04 -0400

    Made carts backend routes -- not yet tested.

---
+++ backend/api/routes/cart.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const Cart = require('../models/Cart.model')
+const auth = require('../../auth')
+const router = express.Router()
+
+// Make new cart
+router.post('/', auth.verifyCustomer, async (req, res, next) => {
+    try {
+        const id = auth.getId(req.cookies.jwt)
+        const cart = new Cart({
+            user: id
+        })
+        await cart.save()
+    } catch(error) {
+        next(error)
+    }
+})
+
+// Delete cart
+router.delete('/', auth.verifyCustomer, async (req, res, next) => {
+    try {
+        const id = auth.getId(req.cookies.jwt)
+        await Cart.deleteOne({user: id})
+    } catch(error) {
+        next(error)
+    }
+})
+
+// Add books to cart
+router.post('/', auth.verifyCustomer, async (req, res, next) => {
+    try {
+        const { bookID, quantity} = req.body
+        const id = auth.getId(req.cookies.jwt)
+        const cart = await Cart.findOne({user : id})
+        cart.books.push({book: bookID, bookQuantity: quantity})
+    } catch(error) {
+        next(error)
+    }
+})
+
+
+// Display all books in cart
+router.get('/', auth.verifyCustomer, async (req, res, next) => {
+    try {
+        const id = auth.getId(req.cookies.jwt)
+        const cart = await Cart.findOne({user : id})
+        res.json(cart.books)
+    } catch(error) {
+        next(error)
+    }
+})
+
+
+// Delete books from cart
+router.delete('/', auth.verifyCustomer, async (req, res, next) => {
+    try {
+        const { bookID, quantity} = req.body
+        const id = auth.getId(req.cookies.jwt)
+        const cart = await Cart.findOne({user : id})
+        cart.books.pop({book: bookID, bookQuantity: quantity})
+    } catch(error) {
+        next(error)
+    }
+})
+
+module.exports = router
