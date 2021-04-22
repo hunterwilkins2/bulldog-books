84afca32474a08621f4f00e3bf6199a6f41da54e
Hunter Wilkins <hunter.wilkins2@gmail.com>
2021-04-22 08:38:33 -0400

    Added route to update cart quantity

--- backend/api/routes/cart.js
+++ backend/api/routes/cart.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const Cart = require('../models/Cart.model')
 const auth = require('../../auth')
-
+const Book = require('../models/Book.model')
 const router = express.Router()
 
 // Add books to cart
@@ -14,7 +14,12 @@ router.post('/', auth.verifyCustomer, async (req, res, next) => {
             if(err)
                 return false
 
-            doc.books.push({ book: bookID, bookQuantity: quantity })
+            const index = doc.books.findIndex(element => element.book === bookID)
+            if(index != -1) {
+                doc.books.splice(index, 1, { book: bookID, bookQuantity: quantity + doc.books[index].bookQuantity })
+            } else {
+                doc.books.push({ book: bookID, bookQuantity: quantity })
+            }
             doc.save()
         })
 
@@ -30,7 +35,48 @@ router.get('/', auth.verifyCustomer, async (req, res, next) => {
     try {
         const id = auth.getId(req.cookies.jwt)
         const cart = await Cart.findOne({ user: id})
-        res.json(cart.books)
+
+        const books = []
+        for(var cartItem of cart.books) {
+            books.push({ book: await Book.findById(cartItem.book), bookQuantity: cartItem.bookQuantity })
+        }
+
+        res.json(books)
+    } catch(error) {
+        next(error)
+    }
+})
+
+// Update quantity of books in cart
+router.patch('/', auth.verifyCustomer, async (req, res, next) => {
+    try {
+        const { bookID, quantity} = req.body
+        const id = auth.getId(req.cookies.jwt)
+        let hadError = false
+        await Cart.findOne({ user: id }, function(err, doc) {
+            if(err)
+                return false
+
+            if(doc.books.length == 0) {
+                hadError = true
+                return false
+            }
+            
+            const index = doc.books.findIndex(element => element.book === bookID)
+            if(index == -1) {
+                hadError = true
+                return false
+            }
+
+            doc.books.splice(index, 1, { book: bookID, bookQuantity: quantity })
+            doc.save()
+        })
+
+        if(hadError){
+            throw Error('Could not update the quantity of the cart item')
+        }
+
+        res.json({ message: 'The quantity was successfully updated' })
     } catch(error) {
         next(error)
     }
