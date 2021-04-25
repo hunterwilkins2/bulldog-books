dbba4d5bfcf758399f48aa8e3b18284916667827
Spencer King <spencerd.king@gmail.com>
2021-04-25 18:23:00 -0400

    Added validation to edit books page

--- src/components/admin/EditBook.js
+++ src/components/admin/EditBook.js
@@ -2,11 +2,61 @@ import React from 'react'
 import { Form, Button, Col } from 'react-bootstrap'
 import { Formik } from 'formik'
 import { Link } from 'react-router-dom'
+import * as yup from 'yup'
+import moment from 'moment'
 
 import StoreNavbar from '../StoreNavbar'
 import '../styles/EditBook.css'
 
 function EditBook (book) {
+
+    const currentDate = moment().format('YYYY-MM-DD')
+    console.log(currentDate)
+
+    const validationSchema = yup.object().shape({
+        title: yup.string()
+            .min(1, 'Title must be betwen 1 and 500 characters')
+            .max(500, 'Title must be betwen 1 and 500 characters'),
+        author: yup.string()
+            .min(1, 'Author must be betwen 1 and 200 characters')
+            .max(200, 'Author must be betwen 1 and 200 characters'),
+        edition: yup.string()
+            .min(1, 'Edition must be betwen 1 and 10 characters')
+            .max(10, 'Edition must be betwen 1 and 10 characters'),
+        category: yup.string()
+            .min(1, 'Category must be betwen 1 and 100 characters')
+            .max(100, 'Category must be betwen 1 and 100 characters'),
+        cover: yup.string()
+            .min(1, 'Cover must be betwen 1 and 200 characters')
+            .max(500, 'Cover must be betwen 1 and 200 characters'),
+        isbn: yup.string()
+            .min(10, 'ISBN must be betwen 10 and 13 characters')
+            .max(13, 'ISBN must be betwen 10 and 13 characters')
+            .matches('^[0-9]*$', 'Can only contain numbers'),
+        quantity: yup.string()
+            .min(1, 'Quantity must be betwen 1 and 6 digits')
+            .max(6, 'Quantity must be betwen 1 and 6 digits')
+            .matches('^[0-9]*$', 'Can only contain numbers'),
+        threshold: yup.string()
+            .min(1, 'ISBN must be betwen 1 and 6 digits')
+            .max(6, 'ISBN must be betwen 1 and 6 digits')
+            .matches('^[0-9]*$', 'Can only contain numbers'),
+        buyPrice: yup.string()
+            .min(1, 'buyPrice must be betwen 1 and 10 digits')
+            .max(10, 'buyPrice must be betwen 1 and 10 digits')
+            .matches('^[0-9 & .]*$', 'Can only contain numbers and decimals'),
+        sellPrice: yup.string()
+            .min(1, 'sellPrice must be betwen 1 and 10 digits')
+            .max(10, 'sellPrice must be betwen 1 and 10 digits')
+            .matches('^[0-9 & .]*$', 'Can only contain numbers and decimals'),
+        publicationDate: yup.date()
+            .max(currentDate, 'Cannot choose a future date'),
+        publisher: yup.string()
+            .min(1, 'Publisher must be betwen 1 and 200 characters')
+            .max(200, 'Publisher must be betwen 1 and 200 characters')
+
+    })
+
     
     return (
         <>
@@ -58,17 +108,14 @@ function EditBook (book) {
                         else {
                             console.log('no errors')
                         }    
-
-
-
                     }}
-                >{({ handleSubmit,
+                    validationSchema={validationSchema}
+                >{({    handleSubmit,
                         handleChange,
                         handleBlur,
                         values,
-                        // touched,
-                        // errors,
-                        // setSubmitting
+                        touched,
+                        errors,
                     }) => (
                         <Form className="login-form" id = "form-edbks" onSubmit={handleSubmit}>
                             <h1>Edit Book</h1>
@@ -82,6 +129,8 @@ function EditBook (book) {
                                             placeholder = {book.history.location.state.book.title}
                                             onChange={handleChange}
                                             onBlur={handleBlur}
+                                            isValid={touched.title && !errors.title}
+                                            isInvalid={touched.title && errors.title}
                                         />
                                     </Form.Group>
                                     <Form.Group >
@@ -92,6 +141,9 @@ function EditBook (book) {
                                             placeholder = {book.history.location.state.book.author}
                                             onChange={handleChange}
                                             onBlur={handleBlur}
+                                            isValid={touched.author && !errors.author}
+                                            isInvalid={touched.author && errors.author} 
+    
                                         />
                                     </Form.Group>
                                     <Form.Group >
@@ -102,6 +154,9 @@ function EditBook (book) {
                                             placeholder = {book.history.location.state.book.edition}
                                             onChange={handleChange}
                                             onBlur={handleBlur}
+                                            isValid={touched.edition && !errors.edition}
+                                            isInvalid={touched.edition && errors.edition} 
+    
                                         />
                                     </Form.Group>
                                     <Form.Group >
@@ -112,6 +167,9 @@ function EditBook (book) {
                                             placeholder = {book.history.location.state.book.category}
                                             onChange={handleChange}
                                             onBlur={handleBlur}
+                                            isValid={touched.category && !errors.category}
+                                            isInvalid={touched.category && errors.category} 
+    
                                         />
                                     </Form.Group>
                                     <Form.Group >
@@ -122,6 +180,9 @@ function EditBook (book) {
                                             placeholder = {book.history.location.state.book.cover}
                                             onChange={handleChange}
                                             onBlur={handleBlur}
+                                            isValid={touched.cover && !errors.cover}
+                                            isInvalid={touched.cover && errors.cover} 
+    
                                         />
                                     </Form.Group>
                                     <Form.Group >
@@ -132,6 +193,9 @@ function EditBook (book) {
                                             placeholder = {book.history.location.state.book.isbn}
                                             onChange={handleChange}
                                             onBlur={handleBlur}
+                                            isValid={touched.isbn && !errors.isbn}
+                                            isInvalid={touched.isbn && errors.isbn} 
+    
                                         />
                                     </Form.Group>
                                 </Col>
@@ -144,6 +208,9 @@ function EditBook (book) {
                                             placeholder = {book.history.location.state.book.quantity}
                                             onChange={handleChange}
                                             onBlur={handleBlur}
+                                            isValid={touched.quantity && !errors.quantity}
+                                            isInvalid={touched.quantity && errors.quantity} 
+    
                                         />
                                     </Form.Group>
                                     <Form.Label>Buy Price</Form.Label>
@@ -154,6 +221,9 @@ function EditBook (book) {
                                             placeholder = {book.history.location.state.book.buyPrice}
                                             onChange={handleChange}
                                             onBlur={handleBlur}
+                                            isValid={touched.buyPrice && !errors.buyPrice}
+                                            isInvalid={touched.buyPrice && errors.buyPrice} 
+    
                                         />
                                     </Form.Group>
                                     <Form.Label>Sell Price</Form.Label>
@@ -164,6 +234,9 @@ function EditBook (book) {
                                             placeholder = {book.history.location.state.book.sellPrice}
                                             onChange={handleChange}
                                             onBlur={handleBlur}
+                                            isValid={touched.sellPrice && !errors.sellPrice}
+                                            isInvalid={touched.sellPrice && errors.sellPrice}
+
                                         />
                                     </Form.Group>
                                     <Form.Label>Publication Date</Form.Label>
@@ -174,6 +247,9 @@ function EditBook (book) {
                                             value={values.publicationDate}
                                             onChange={handleChange}
                                             onBlur={handleBlur}
+                                            isValid={touched.publicationDate && !errors.publicationDate}
+                                            isInvalid={touched.publicationDate && errors.publicationDate} 
+
                                         />
                                     </Form.Group>
                                     <Form.Label>Publisher</Form.Label>
@@ -184,6 +260,9 @@ function EditBook (book) {
                                             placeholder = {book.history.location.state.book.publisher}
                                             onChange={handleChange}
                                             onBlur={handleBlur}
+                                            isValid={touched.publisher && !errors.publisher}
+                                            isInvalid={touched.publisher && errors.publisher} 
+    
                                         />
                                     </Form.Group>
                                     <Form.Label>Threshold Value</Form.Label>
@@ -194,6 +273,9 @@ function EditBook (book) {
                                             placeholder = {book.history.location.state.book.threshold}
                                             onChange={handleChange}
                                             onBlur={handleBlur}
+                                            isValid={touched.threshold && !errors.threshold}
+                                            isInvalid={touched.threshold && errors.threshold} 
+    
                                         />
                                     </Form.Group>
                                 </Col>
